4c78264c0970a1f9102ee676efdafef4592fae47
andrewaltman1 <55675406+andrewaltman1@users.noreply.github.com>
2023-03-28 20:14:57 -0400

    working on state

--- client/App.js
+++ client/App.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import Remote from './components/Remote';
 import Button from './components/Button';
 import Overlay from './components/Overlay';
 //import styles if necessary
 
+const initalState = {
+  isMigrating: false,
+  origin: { name: '', accessId: '', secretKey: '' },
+  destination: {
+    name: '',
+    secretKey: '',
+    accessId: '',
+    accountId: '',
+  },
+
+  //     AWS: Access ID, Secret Key, and "Service" aka S3
+
+  // 12:27
+  // CF: Access Id, Secret Key, and Account ID
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'add':
+      return [
+        ...state,
+        {
+          id: state.length,
+          name: action.name,
+        },
+      ];
+    default:
+      return state;
+  }
+}
+
 const App = (props) => {
+  const [state, dispatch] = useReducer(reducer, { age: 42 });
   //Some Things to track in State...
   //isMigrating / loading status
   //From/To component input fields
