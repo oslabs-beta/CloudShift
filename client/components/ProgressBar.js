1ccc729ffe49493c82cc85ebbdb06a57718af926
Phil <phil.luc89@gmail.comm>
2023-04-06 15:25:05 -0400

    progress

--- client/components/ProgressBar.js
+++ client/components/ProgressBar.js
@@ -19,20 +19,20 @@ const ProgressBar = () => {
     <div className="relative pt-1 w-2/5 h-screen">
       <div className="flex mb-2 items-center justify-between">
         <div>
-          <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-stone-100 bg-emerald-300">
+          <span className="text-l font-semibold inline-block py-1 px-2 uppercase rounded-full text-stone-100 bg-cyan-400">
             Migration in progress
           </span>
         </div>
         <div className="text-right">
-          <span className="text-xs font-semibold inline-block text-black-600">
+          <span className="text-s font-semibold inline-block text-black-600">
             {transferVal}
           </span>
         </div>
       </div>
-      <div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-emerald-100 w-full">
+      <div className="overflow-hidden h-6 mb-4 text-xs flex rounded-2xl bg-cyan-200 w-full">
         <div
-          style={{ width: transferVal }}
-          className={`shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-emerald-300`}
+          style={{ width: transferVal}}
+          className={`shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-cyan-800`}
         ></div>
       </div>
     </div>
