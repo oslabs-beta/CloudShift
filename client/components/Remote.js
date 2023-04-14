00b0f9c82cf309f757c673bcdad679c1800b5bd9
mjhuff <mjhuff@users.noreply.github.com>
2023-04-14 21:36:50 +0000

    Prettified Code!

--- client/components/Remote.js
+++ client/components/Remote.js
@@ -1,10 +1,10 @@
-import React, { useEffect } from 'react';
-import { updateSecretKey, updateAccessId, updateAccountId } from '../slice';
-import { useDispatch, useSelector } from 'react-redux';
-import BucketSelect from './BucketSelect';
-import { getUserBuckets } from '../services/getBuckets';
-import ErrorDisplay from './ErrorDisplay';
-import StartMigrationButton from './MigrationButton';
+import React, { useEffect } from "react";
+import { updateSecretKey, updateAccessId, updateAccountId } from "../slice";
+import { useDispatch, useSelector } from "react-redux";
+import BucketSelect from "./BucketSelect";
+import { getUserBuckets } from "../services/getBuckets";
+import ErrorDisplay from "./ErrorDisplay";
+import StartMigrationButton from "./MigrationButton";
 
 const Remote = (props) => {
   const dispatch = useDispatch();
@@ -12,11 +12,11 @@ const Remote = (props) => {
 
   let { remoteType } = props;
 
-  const remote = remoteType === 'origin' ? origin : destination;
+  const remote = remoteType === "origin" ? origin : destination;
 
   let bucketSelect;
 
-  const requireAccountId = remote.name === 'Cloudflare' ? true : false;
+  const requireAccountId = remote.name === "Cloudflare" ? true : false;
 
   if (!requireAccountId) {
     bucketSelect = remote.accessId && remote.secretKey && (
@@ -31,14 +31,14 @@ const Remote = (props) => {
   //Get the list of buckets if all credentials are present.
   useEffect(() => {
     if (!remote.accessId || !remote.secretKey) return;
-    if (remote.name === 'Cloudflare' && !remote.accountId) return;
+    if (remote.name === "Cloudflare" && !remote.accountId) return;
     dispatch(getUserBuckets({ ...remote, originOrDestination: remoteType }));
   }, [remote.accessId, remote.secretKey, remote.name, remote.accountId]);
 
   let correctInputClass =
-    'block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-800 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer';
+    "block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-800 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer";
   let wrongInputClass =
-    'block py-2.5 px-0 w-full text-sm text-red-600 bg-transparent border-0 border-b-2 border-red-600 appearance-none focus:outline-none focus:ring-0 focus:border-red-600 peer';
+    "block py-2.5 px-0 w-full text-sm text-red-600 bg-transparent border-0 border-b-2 border-red-600 appearance-none focus:outline-none focus:ring-0 focus:border-red-600 peer";
 
   return (
     <div>
@@ -69,12 +69,12 @@ const Remote = (props) => {
           htmlFor={`${remoteType}AccessId`}
           className={`${
             remote.errorMessage
-              ? 'text-red-600 peer-focus:text-red-600'
-              : 'peer-focus:text-blue-600'
+              ? "text-red-600 peer-focus:text-red-600"
+              : "peer-focus:text-blue-600"
           } peer-focus:font-medium absolute text-base duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6`}
         >
-          {remote.name === 'azureblob' ? 'Account ID' : 'Access ID'}
-          {remote.accessId ? <>{' \u2705'}</> : <></>}
+          {remote.name === "azureblob" ? "Account ID" : "Access ID"}
+          {remote.accessId ? <>{" \u2705"}</> : <></>}
         </label>
       </div>
 
@@ -96,16 +96,16 @@ const Remote = (props) => {
           htmlFor="secretKey"
           className={`${
             remote.errorMessage
-              ? 'text-red-600 peer-focus:text-red-600'
-              : 'peer-focus:text-blue-600'
+              ? "text-red-600 peer-focus:text-red-600"
+              : "peer-focus:text-blue-600"
           } peer-focus:font-medium absolute text-base duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6`}
         >
-          {remote.name === 'azureblob' ? 'Access Key' : 'Secret Key'}
-          {remote.secretKey ? <>{' \u2705'}</> : <></>}
+          {remote.name === "azureblob" ? "Access Key" : "Secret Key"}
+          {remote.secretKey ? <>{" \u2705"}</> : <></>}
         </label>
       </div>
 
-      {remote.name === 'Cloudflare' && (
+      {remote.name === "Cloudflare" && (
         <div className="relative z-0 w-full h-full mb-6 group">
           <input
             className={
@@ -120,7 +120,7 @@ const Remote = (props) => {
               dispatch(
                 updateAccountId({
                   accountId: e.target.value.trim(),
-                  remoteType
+                  remoteType,
                 })
               );
             }}
@@ -129,11 +129,11 @@ const Remote = (props) => {
             htmlFor="accountId"
             className={`${
               remote.errorMessage
-                ? 'text-red-600 peer-focus:text-red-600'
-                : 'peer-focus:text-blue-600'
+                ? "text-red-600 peer-focus:text-red-600"
+                : "peer-focus:text-blue-600"
             } peer-focus:font-medium absolute text-base duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6`}
           >
-            Account ID {remote.accountId.length === 32 && <>{'\u2705'}</>}
+            Account ID {remote.accountId.length === 32 && <>{"\u2705"}</>}
           </label>
         </div>
       )}
@@ -148,7 +148,7 @@ const Remote = (props) => {
 
       {origin.selectedBucket &&
       destination.selectedBucket &&
-      remoteType === 'destination' ? (
+      remoteType === "destination" ? (
         <StartMigrationButton></StartMigrationButton>
       ) : null}
     </div>
