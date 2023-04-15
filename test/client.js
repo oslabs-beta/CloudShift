9b9a1bf021ea6c828c58772a729dc26c12715a82
Jamey Huffnagle <mjhuffnagle@gmail.com>
2023-04-14 22:56:37 -0400

    Fixed spelling

--- test/client.js
+++ test/client.js
@@ -12,7 +12,7 @@ import store from '../client/store';
 
 describe('Unit testing React components', () => {
   describe('Renders necessary components.', () => {
-    test('Should render proper Origin/Destination components on initial load.', () => {
+    test('Should render Navbar & Origin/Destination components on initial load.', () => {
       const app = renderer
         .create(
           <Provider store={store}>
