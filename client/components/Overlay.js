2193cdbc8e2f817af5cfe8cc17fc2e8c7d0ee6da
Jamey Huffnagle <mjhuffnagle@gmail.com>
2023-04-14 22:54:08 -0400

    Basic testing boilerplate

--- client/components/Overlay.js
+++ client/components/Overlay.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import aws_edited from "../public/aws_edited.png";
-import cloudflare_edited from "../public/cloudflare_edited.png";
-import ReactDOM from "react-dom";
-import ProgressBar from "./ProgressBar";
+import React from 'react';
+import { useSelector } from 'react-redux';
+import aws_edited from '../public/aws_edited.png';
+import cloudflare_edited from '../public/cloudflare_edited.png';
+import ReactDOM from 'react-dom';
+import ProgressBar from './ProgressBar';
 
 const Overlay = (props) => {
   const { origin, destination } = useSelector((state) => state.GUI);
-  const originSrc = origin.name === "AWS" ? aws_edited : cloudflare_edited;
+  const originSrc = origin.name === 'AWS' ? aws_edited : cloudflare_edited;
   const destinationSrc =
-    destination.name === "AWS" ? aws_edited : cloudflare_edited;
+    destination.name === 'AWS' ? aws_edited : cloudflare_edited;
   return ReactDOM.createPortal(
     <>
       <div className="flex left-0 top-0 justify-center item-center fixed z-9999 h-screen w-screen backdrop-blur-xl">
@@ -18,7 +18,7 @@ const Overlay = (props) => {
         </div>
       </div>
     </>,
-    document.querySelector("#portal")
+    document.querySelector('#portal')
   );
 };
 
