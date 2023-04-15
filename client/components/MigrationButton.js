8609752a93a3a7e58d9efcab0d8ea8502028f291
Phil <phil.luc89@gmail.comm>
2023-04-14 22:43:00 -0400

    added focus

--- client/components/MigrationButton.js
+++ client/components/MigrationButton.js
@@ -20,6 +20,7 @@ const StartMigrationButton = () => {
     <>
       <div className="flex justify-center items-center mt-8">
         <button
+        autoFocus
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
           onClick={() => dispatch(migrationStatusChange(true))}
         >
