efc6882d188f4c9bef8ccf0834637b0b59a97003
andrewaltman1 <55675406+andrewaltman1@users.noreply.github.com>
2023-06-12 13:49:22 -0400

    added conditional to check for root render

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -2,14 +2,18 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import store from "./store";
-import App from "./App.js";
+import App from "./App";
 
 //we might need styles here
 
 const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+if (container){
+  const root = createRoot(container);
+  root.render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );} else {
+    throw new ReferenceError("Could not find root element.")
+  }
+
